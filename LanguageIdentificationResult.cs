2d6c89ca53643fd0e729df201394fe141e9545ae
mateuszPATeam <60876661+mateuszPATeam@users.noreply.github.com>
2023-04-21 09:00:42 +0200

    Add project files.

---
+++ LanguageIdentificationResult.cs
@@ -0,0 +1,55 @@
+﻿using Direct.Shared;
+
+namespace NICE_LanguageUtils
+{
+    [DirectDom("Language Identification Result", "General")]
+    public class LanguageIdentificationResult : DirectComponentBase
+    {
+        protected PropertyHolder<string> _EnglishName = new PropertyHolder<string>("English Name");
+        protected PropertyHolder<string> _NativeName = new PropertyHolder<string>("Native Name");
+        protected PropertyHolder<string> _TwoLetterName = new PropertyHolder<string>("Two Letter Name");
+        protected PropertyHolder<string> _ThreeLetterName = new PropertyHolder<string>("Three Letter Name");
+
+        [DirectDom("English Name")]
+        [DesignTimeInfo("English Name")]
+        public string EnglishName
+        {
+            get { return _EnglishName.TypedValue; }
+            set { _EnglishName.TypedValue = value; }
+        }
+
+        [DirectDom("Native Name")]
+        [DesignTimeInfo("Native Name")]
+        public string NativeName
+        {
+            get { return _NativeName.TypedValue; }
+            set { _NativeName.TypedValue = value; }
+        }
+        [DirectDom("Two Letter Name")]
+        [DesignTimeInfo("Two Letter Name")]
+
+        public string TwoLetterName
+        {
+            get { return _TwoLetterName.TypedValue; }
+            set { _TwoLetterName.TypedValue = value; }
+        }
+
+        [DirectDom("Three Letter Name")]
+        [DesignTimeInfo("Three Letter Name")]
+        public string ThreeLetterName
+        {
+            get { return _ThreeLetterName.TypedValue; }
+            set { _ThreeLetterName.TypedValue = value; }
+        }
+
+        public LanguageIdentificationResult()
+        {
+
+        }
+
+        public LanguageIdentificationResult(IProject project) : base(project)
+        {
+        }
+
+    }
+}
---
+++ LanguageUtils.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Direct.Shared;
+using LanguageIdentification;
+using log4net;
+
+namespace NICE_LanguageUtils
+{
+    [DirectSealed]
+    [DirectDom("Language Utils", "General")]
+    [ParameterType(false)]
+    public static class LanguageUtils
+    {
+        private static readonly ILog _log = LogManager.GetLogger("LibraryObjects");
+
+        [DirectDom("Identify Text Language")]
+        [DirectDomMethod("Identify Language of {Text}. Languages Profiles: {Languages Profiles}, Language Idenitification Method: {Language Idenitification Method}")]
+        [MethodDescription("Identifies text language")]
+        public static LanguageIdentificationResult GetTextLanguage(string text, string lanProfilesPath, int lanIdentificationMethod) 
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                _log.Error("NICE-LanguageUtils: Text to recognize is empty");
+            }
+
+            if (string.IsNullOrEmpty(lanProfilesPath))
+            {
+                _log.Error("NICE-LanguageUtils: Languages profiles path is empty");
+            }
+
+            if (lanIdentificationMethod == 0 || lanIdentificationMethod > 4)
+            {
+                _log.Error("NICE-LanguageUtils: Only numbers 1 to 4 are supported for language identification method");
+            }
+
+            if (_log.IsDebugEnabled)
+            {
+                _log.Debug("NICE-LanguageUtils: Getting identification method");
+            }
+
+            string identificationMethod = GetIdentificationMethod(lanIdentificationMethod);
+
+            try
+            {
+                if (_log.IsDebugEnabled)
+                {
+                    _log.Debug("NICE-LanguageUtils: Building Language Identifier");
+                }
+
+                LanguageIdentifier languageIdentifier = LanguageIdentifier.New(lanProfilesPath, identificationMethod, -1);
+
+                if (_log.IsDebugEnabled)
+                {
+                    _log.Debug("NICE-LanguageUtils: Identifying language of text");
+                }
+                string identificationResult = languageIdentifier.Identify(text);
+
+                CultureInfo culture = new CultureInfo(identificationResult);
+
+
+                if (_log.IsDebugEnabled)
+                {
+                    _log.Debug("NICE-LanguageUtils: Returning result");
+                }
+
+                return new LanguageIdentificationResult()
+                {
+                    EnglishName = culture.EnglishName,
+                    NativeName = culture.NativeName,
+                    TwoLetterName = culture.TwoLetterISOLanguageName,
+                    ThreeLetterName = culture.ThreeLetterISOLanguageName,
+                };
+
+            }
+            catch (Exception e)
+            {
+                _log.Error("NICE-LanguageUtils: Unexpected error", e);
+                return new LanguageIdentificationResult();
+            }
+        }
+
+        private static string GetIdentificationMethod(int method)
+        {
+            switch (method) 
+            {
+                case 1:
+                    return "Vector";
+                case 2:
+                    return "Likely";
+                case 3:
+                    return "Rank,sf";
+                case 4:
+                    return "Rank,sr";
+                default:
+                    return "";
+            }
+        }
+    }
+}
